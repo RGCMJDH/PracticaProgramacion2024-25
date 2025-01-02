f9ff130a5b98275ef6deca37ad02e85cd3ff5e39
caarlosalemaany <151624715+caarlosalemaany@users.noreply.github.com>
2025-01-02 14:14:04 +0100

    Add files via upload

--- PracticaAlemanyGallego.java
+++ PracticaAlemanyGallego.java
@@ -17,7 +17,7 @@ public class PracticaAlemanyGallego {
     public static void main(String[] args) {
         // TODO code application logic here
         Tablero prueba = new Tablero();
-        System.out.println(prueba.TaulerToString());
+        System.out.println(prueba.imprimirTablero());
              
     }
     
