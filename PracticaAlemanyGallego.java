28105713aac7401ed28660133b9a6e88ae7d2afb
RGCMJDH <albertgallegodiaz@gmail.com>
2025-01-13 18:01:09 +0100

    Update PracticaAlemanyGallego.java

--- PracticaAlemanyGallego.java
+++ PracticaAlemanyGallego.java
@@ -8,20 +8,18 @@ package practicaalemanygallego;
  *
  * @author cresp
  */
-public class PracticaAlemanyGallego {
-
-    /**
-     * @param args the command line arguments
-     */
-   
-    public static void main(String[] args) {
+public static void main(String[] args) {
         // TODO code application logic here
         Tablero prueba = new Tablero();
         System.out.println(prueba.imprimirTablero());
-        
-        TableroJ jugador = new TableroJ();
-        System.out.println(jugador.imprimirTableroJ());
-             
+
+         Utilitats util = new Utilitats();
+        int tamTablero = util.validarEntradaSencer("Introduce un tamaño", "Tamaño no válido, dame otro");
+
+        char[] nombre = util.validarEntradaLiniaC("Nombre del Jugador?", "Nombre Inválido, dame otro");
+        System.out.println(util.toString(nombre));
+
+        Tablero jugador = new Tablero(util.toString(nombre), tamTablero);
+        System.out.println(jugador.imprimirTablero());
+
     }
-    
-}
